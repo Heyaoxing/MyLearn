db2ad113adb85f30439dc5a13b76303033d95c8e
Heyaoxing <410842694@qq.com>
2016-07-07 17:10:01 +0800

    增加Lambda项目

--- 语法/MyLearn.Lambda/LBAction.cs
+++ 语法/MyLearn.Lambda/LBAction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace MyLearn.Lambda
 {
     public class LBAction
     {
+      public  Action NotParamAction = () =>
+        {
+            Trace.WriteLine("即时窗口显示");
+        };
 
+      public Action<int, int> ParamAction = (x, y) =>
+        {
+            Trace.WriteLine("两个数相加等于:"+(x+y));
+        };
     }
 }
