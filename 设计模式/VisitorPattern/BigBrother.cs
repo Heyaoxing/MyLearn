c5ae4d627feeb1635a0eaeb2278fc6e3c70ff89b
730530507 <730530507@qq.com>
2017-03-01 15:50:56 +0800

    1

---
+++ 设计模式/VisitorPattern/BigBrother.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisitorPattern
+{
+    /// <summary>
+    /// 大哥
+    /// </summary>
+    public class BigBrother
+    {
+        public void RedEnvelopes(string name)
+        {
+            Console.WriteLine(name + "恭喜发财");
+        }
+    }
+
+    /// <summary>
+    /// 弟弟
+    /// </summary>
+    public class LittleBrother
+    {
+        public void RedEnvelopes(string name)
+        {
+            Console.WriteLine(name + "恭喜发财");
+        }
+    }
+}
